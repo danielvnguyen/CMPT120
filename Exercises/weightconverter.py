118a3a5e5d1038e97cf7d74fc99c8f3ba32a0853
Daniel Nguyen <danielnguyen604@gmail.com>
2021-09-02 02:48:38 -0700

    Create weightconverter.py

---
+++ Exercises/weightconverter.py
@@ -0,0 +1,11 @@
+# Weight Converter
+# Daniel Nguyen
+# Sept 30th, 2020
+
+# Get weight in pounds
+pounds = float(input("How many pounds? "))
+
+# Output weight in kilos
+kg = pounds*0.45
+
+print("That's " + str(kg) + "kg.")
