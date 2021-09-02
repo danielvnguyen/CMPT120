8aab4b7f2e0e8b8e65403f0fc82e0186bd6ebe78
Daniel Nguyen <danielnguyen604@gmail.com>
2021-09-02 02:39:24 -0700

    Create motivational_quote.py

---
+++ Exercises/motivational_quote.py
@@ -0,0 +1,6 @@
+# Title: Motivational Quote Generator
+# Author: Daniel Nguyen
+# Date: September 11th, 2020
+
+# Output the quote to the screen
+print ("“The more you focus on you, the better you become.” Doe Zantamata")
